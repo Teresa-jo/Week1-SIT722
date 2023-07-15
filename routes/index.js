f6cd365d0af07053ac745ddf8d769615fbb31819
Jo <s222186709@deakin.edu.au>
2023-07-15 14:18:37 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 const nameList = [
-{ 'id': 1, 'name': 'John', 'email': 'aaa'},
-{'id': 2, 'name': 'James', 'email': 'abc'},
-{'id': 100, 'name': 'David', 'email': 'adc'},
+  { 'id': 1, 'name': 'John', 'email': 'aaa' },
+  { 'id': 2, 'name': 'James', 'email': 'abc' },
+  { 'id': 3, 'name': 'David', 'email': 'adc' },
+  { 'id': 4, 'name': 'David', 'email': 'adc' },
+  { 'id': 5, 'name': 'David', 'email': 'adc' },
+  { 'id': 6, 'name': 'David', 'email': 'adc' },
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SIT722', sub: 'ABC', names: nameList  });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'SIT722 DevOps Glossary',
+    sub: 'my website showing a collection of DevOps terms and their brief descriptions',
+    names: nameList
+  });
 });
 
 module.exports = router;
