fecc2339676c55d05273aae01d8b96e7b1c289ca
Jo <s222186709@deakin.edu.au>
2023-07-15 15:49:33 +1000

    Added 10 terms

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const nameList = [
-  { 'id': 1, 'name': 'John', 'email': 'aaa' },
-  { 'id': 2, 'name': 'James', 'email': 'abc' },
-  { 'id': 3, 'name': 'David', 'email': 'adc' },
-  { 'id': 4, 'name': 'David', 'email': 'adc' },
-  { 'id': 5, 'name': 'David', 'email': 'adc' },
-  { 'id': 6, 'name': 'David', 'email': 'adc' },
-];
-
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'SIT722 DevOps Glossary',
-    sub: 'my website showing a collection of DevOps terms and their brief descriptions',
-    names: nameList
-  });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'SIT722 DevOps Glossary', 
+  sub: 'my website showing a collection of DevOps terms and their brief descriptions.', names: nameList  });
 });
 
 module.exports = router;
