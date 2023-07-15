3b4485c2135dec18b7519fdfbc78e6edceb93e98
Jo <s222186709@deakin.edu.au>
2023-07-15 19:53:39 +1000

    Changed file name

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'SIT722 DevOps Glossary', 
-  sub: 'my website showing a collection of DevOps terms and their brief descriptions.', names: nameList  });
+  sub: 'my website showing a collection of DevOps terms and their brief descriptions.', 
+  name: 'Please find my Git website here: https://github.com/Teresa-jo/sit722-2023-t2-glossary'  });
 });
 
 module.exports = router;
