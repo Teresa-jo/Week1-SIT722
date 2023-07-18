728ab54477aed5197d5fe62800917007e4247aab
Jo <s222186709@deakin.edu.au>
2023-07-18 10:50:20 +1000

    changed

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SIT722 DevOps Glossary', 
-  sub: 'my website showing a collection of DevOps terms and their brief descriptions.', names: nameList  });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'SIT722 DevOps Glossary',
+    sub: 'my website showing a collection of DevOps terms and their brief descriptions.', 
+    name: 'Please find my Git website here: https://github.com/Teresa-jo/week_01'
+  });
 });
 
 module.exports = router;
